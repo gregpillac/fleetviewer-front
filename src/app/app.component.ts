07acef04b6c925f9a4d0f00f39791fd1f1e68b84
vnctsly <97257498+Dekaem@users.noreply.github.com>
2025-05-27 11:27:36 +0200

    Testing User API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {SvgIconComponent} from 'angular-svg-icon';
+import {User, UserService} from './services/user.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SvgIconComponent],
+  imports: [RouterOutlet, SvgIconComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'fleetviewer-front';
+  users: User[] = [];
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe({
+      next: (data) => this.users = data,
+      error: (err) => console.error('Erreur de chargement des utilisateurs', err)
+    });
+  }
 }
