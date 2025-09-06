7fc6281c82590ddc5e370ae52fb20b450771c5b8
eperia35 <thomas.huet@hotmail.fr>
2025-09-06 11:55:11 +0200

    ajout reservation

--- src/app/pages/ride-search/ride-search.component.ts
+++ src/app/pages/ride-search/ride-search.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { ConfirmationDialogComponent } from '../reservations/confirmation-dialog/confirmation-dialog.component';
 
 @Component({
   selector: 'app-ride-search',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, MatTabsModule, MatButtonModule, MatDialogModule, ConfirmationDialogComponent],
   templateUrl: './ride-search.component.html',
-  styleUrl: './ride-search.component.scss'
+  styleUrls: ['./ride-search.component.scss']
 })
 export class RideSearchComponent {
+  constructor(private dialog: MatDialog) {}
 
+  reserverVehicule() {
+    const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+      width: '400px',
+      data: {
+        message: 'Un trajet en covoiturage similaire existe. Voulez-vous continuer avec le véhicule ?'
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        console.log("✅ Réservation de véhicule confirmée !");
+      } else {
+        console.log("🚗 Voir le covoiturage");
+      }
+    });
+  }
 }
